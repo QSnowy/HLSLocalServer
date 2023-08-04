50295011552bd27aa0c06ada6711b6688a87d874
xuequan <xuequan18@dingtalk.com>
2023-08-04 09:42:41 +0800

    Initial Commit

---
+++ HLSLocalServer/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  HLSLocalServer
+//
+//  Created by xuequan on 2023/8/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ HLSLocalServer/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  HLSLocalServer
+//
+//  Created by xuequan on 2023/8/4.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
